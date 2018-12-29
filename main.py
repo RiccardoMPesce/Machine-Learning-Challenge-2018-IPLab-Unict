cf664373c9385ca576dbd7d23167bb1189c8e6bc
riccardompesce <riccardompesce@gmail.com>
2018-12-29 08:16:43 +0100

    Add import Variable, since we need to wrap all our tensor into variable so as to make them differentiable

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import mlcdataset as mlc
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.optim import SGD
+from torch.autograd import Variable
 
 class Net(nn.Module):
     def __init__(self):
@@ -60,8 +61,37 @@ else:
     call["test_samples"] = int(sys.argv[3])
     call["labeled_source_path"] = sys.argv[4]
     call["unlabeled_source_path"] = sys.argv[5]
+    
     makelist.make_list(**call)
 
-cnn = Net()
+net = Net()
 
-training_data = mlc.MLCDataset
+# Training the model 
+training_set = mlc.MLCDataset("dataset/images", "training_set.csv", transform=mlc.normalization)
+training_set_loader = DataLoader(dataset=training_set, batch_size=32, num_workers=2, shuffle=True)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = SGD(net.parameters(), lr=0.01, momentum=0.9)
+
+epochs = 10
+
+for epoch in range(epochs):
+    loss = 0.0
+
+    for i, data in enumerate(training_set_loader):
+        inputs, labels = data 
+
+        # reset the gradient
+        optimizer.zero_grad()
+
+        # feed-forward -> back-propagation -> optimization
+        outputs = net(inputs)
+        cost = criterion(outputs, labels)
+        cost.backward()
+        optimizer.step()
+
+        # stats
+        loss += cost.item()
+
+        if i % 10 == 0:
+            print str(epoch + 1) + ": " + str(i + 1) + " " + str(loss / sys.argv[1]) 
