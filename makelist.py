0f13c6353ce92d45961289170b2ab92b7acaf33a
riccardompesce <riccardompesce@gmail.com>
2018-12-30 21:00:38 +0100

    Popping first element

--- makelist.py
+++ makelist.py
@@ -20,17 +20,17 @@ def make_list(training_samples, validation_samples, test_samples,
     with open("training_set.csv", "w") as training:
         for i in range(training_samples):
             shuffle(training_list)
-            temp = training_list.pop(i)
+            temp = training_list.pop(0)
             training.write(temp[0] + ", " + str(temp[1]) + "\n")
     
     with open("validation_set.csv", "w") as validation:
         for i in range(validation_samples):
             shuffle(validation_list)
-            temp = training_list.pop(i)
+            temp = training_list.pop(0)
             validation.write(temp[0] + ", " + str(temp[1]) + "\n")
 
     with open("test_set.csv", "w") as test:
         for i in range(test_samples):
             shuffle(test_list)
-            temp = test_list.pop(i)
+            temp = test_list.pop(0)
             test.write(temp[0] + "\n")
