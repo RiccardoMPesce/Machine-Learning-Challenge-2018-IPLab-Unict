93e55357b7b97fa5908740b1122f2ba6bbacc45e
riccardompesce <riccardompesce@gmail.com>
2018-12-30 21:59:09 +0100

    Corrected

--- makelist.py
+++ makelist.py
@@ -17,19 +17,19 @@ def make_list(training_samples, validation_samples, test_samples,
     validation_samples = len(validation_list) if validation_samples > len(validation_list) else validation_samples
     test_samples = len(test_list) if test_samples > len(test_list) else test_samples
 
-    with open("training_set.csv", "w") as training:
+    with open(training_source_path, "w") as training:
         for i in range(training_samples):
             shuffle(training_list)
             temp = training_list.pop(0)
             training.write(temp[0] + ", " + str(temp[1]) + "\n")
     
-    with open("validation_set.csv", "w") as validation:
+    with open(validation_source_path, "w") as validation:
         for i in range(validation_samples):
             shuffle(validation_list)
             temp = training_list.pop(0)
             validation.write(temp[0] + ", " + str(temp[1]) + "\n")
 
-    with open("test_set.csv", "w") as test:
+    with open(test_source_path, "w") as test:
         for i in range(test_samples):
             shuffle(test_list)
             temp = test_list.pop(0)
