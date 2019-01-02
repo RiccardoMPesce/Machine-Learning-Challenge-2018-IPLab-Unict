cfeec94d2ad6017ddbd87f8980723b28723c389e
riccardompesce <riccardompesce@gmail.com>
2019-01-02 11:51:18 +0100

    Added percentage to accuracy_score

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def train_model(model=resnet_model, optimizer=optimizer, epochs=N_EPOCHS, moment
             loss.backward()
 
             epoch_loss += loss.data.mean()
-            epoch_accuracy += accuracy_score(y, output.max(1)[0])
+            epoch_accuracy += accuracy_score(y.data, output.max(1)[1].data)
 
             optimizer.step()
 
